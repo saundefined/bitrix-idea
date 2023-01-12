f7a4e78c1c961ecaad28504c6ec01561b851d78f
GitHub Action <action@github.com>
2023-01-12 19:47:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/saundefined/bitrixidea/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/saundefined/bitrixidea/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.saundefined.bitrixidea.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.saundefined.bitrixidea.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/saundefined/bitrixidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/saundefined/bitrixidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.saundefined.bitrixidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.saundefined.bitrixidea.MyBundle
 
 class MyApplicationService {
 
