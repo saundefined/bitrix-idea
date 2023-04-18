218655fb15f654bfde0bc34a902dcf7950a81cd3
Sergey Panteleev <sergey@php.net>
2023-04-18 18:58:50 +0300

    feat: Add startup Notification

---
+++ src/main/kotlin/com/github/saundefined/bitrix_idea/BitrixIdeaBundle.kt
@@ -0,0 +1,20 @@
+package com.github.saundefined.bitrix_idea
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+
+@NonNls
+private const val BUNDLE = "messages.BitrixIdeaBundle"
+
+object BitrixIdeaBundle : DynamicBundle(BUNDLE) {
+    @Suppress("SpreadOperator")
+    @JvmStatic
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
+        getMessage(key, *params)
+
+    @Suppress("SpreadOperator", "unused")
+    @JvmStatic
+    fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
+        getLazyMessage(key, *params)
+}
---
+++ src/main/kotlin/com/github/saundefined/bitrix_idea/NotificationStartupActivity.kt
@@ -0,0 +1,27 @@
+package com.github.saundefined.bitrix_idea
+
+import com.github.saundefined.bitrix_idea.BitrixIdeaBundle.message
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+
+class NotificationStartupActivity : StartupActivity {
+
+    override fun runActivity(project: Project) {
+        val app = ApplicationManager.getApplication()
+        app.invokeLater {
+            val notification = Notification(
+                "Bitrix Idea Plugin",
+                message("startup_notification_title"),
+                message("startup_notification_body"),
+                NotificationType.INFORMATION
+            )
+
+            Notifications.Bus.notify(notification)
+        }
+    }
+
+}
