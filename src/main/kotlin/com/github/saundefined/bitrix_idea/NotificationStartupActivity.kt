96ff4627b7f3f9ee90fb303c30fc19c0c8e7416a
Sergey Panteleev <sergey@php.net>
2023-04-18 20:28:44 +0300

    feat: Add freemium functionality

--- src/main/kotlin/com/github/saundefined/bitrix_idea/NotificationStartupActivity.kt
+++ src/main/kotlin/com/github/saundefined/bitrix_idea/NotificationStartupActivity.kt
@@ -1,6 +1,7 @@
 package com.github.saundefined.bitrix_idea
 
 import com.github.saundefined.bitrix_idea.BitrixIdeaBundle.message
+import com.github.saundefined.bitrix_idea.license.CheckLicense
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
@@ -9,18 +10,19 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 
 class NotificationStartupActivity : StartupActivity {
-
     override fun runActivity(project: Project) {
         val app = ApplicationManager.getApplication()
         app.invokeLater {
-            val notification = Notification(
-                "Bitrix Idea Plugin",
-                message("startup_notification_title"),
-                message("startup_notification_body"),
-                NotificationType.INFORMATION
-            )
+            if (CheckLicense.isLicensed == false) {
+                val notification = Notification(
+                    "Bitrix Idea Plugin",
+                    message("startup_notification_title"),
+                    message("startup_notification_body"),
+                    NotificationType.INFORMATION
+                )
 
-            Notifications.Bus.notify(notification)
+                Notifications.Bus.notify(notification)
+            }
         }
     }
 
